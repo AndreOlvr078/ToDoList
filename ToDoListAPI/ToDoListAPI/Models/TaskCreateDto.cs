a7c0c9f8dec1f3a99e811f2bc2c32b1ee6b5667e
AndreOlvr078 <andrea.onyrix.com@gmail.com>
2025-06-19 09:45:49 +0200

    Stato passato da string a bool

--- ToDoListAPI/ToDoListAPI/Models/TaskCreateDto.cs
+++ ToDoListAPI/ToDoListAPI/Models/TaskCreateDto.cs
@@ -6,7 +6,7 @@
         public string Titolo { get; set; }
         public string? Descrizione { get; set; }
         public DateTime? Scadenza { get; set; }
-        public string Stato { get; set; }
+        public bool Stato { get; set; }
         public int? CategoriaID { get; set; }
         public int? UtenteID { get; set; }
     }
--- ToDoListAPI/ToDoListAPI/Models/TaskJoinDto.cs
+++ ToDoListAPI/ToDoListAPI/Models/TaskJoinDto.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string Titolo { get; set; }
         public string Descrizione { get; set; }
-        public DateTime Scadenza { get; set; }
-        public string Stato { get; set; }
+        public DateTime? Scadenza { get; set; }
+        public bool Stato { get; set; }
         public string Categoria { get; set; }
         public string Utente { get; set; }
     }
