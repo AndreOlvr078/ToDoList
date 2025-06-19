9b343b4649bcc05fcbe5c0c3489928d91042c8fa
GabrySilve <gabriele.silvestri@itismajo.onmicrosoft.com>
2025-06-19 11:12:49 +0200

    url aggiunto

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ function salvaTask(e) {
     method = 'PUT';
   }
 
-  fetch(url, {
+  fetch(url =`https://localhost:7000/api/Task/${taskDaModificare}`, {
     method,
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
